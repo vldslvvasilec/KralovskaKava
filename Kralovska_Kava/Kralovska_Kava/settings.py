ed96e655f9b2c6e583ba6e477aea3c723ecba35e
Vlad Ponomarov <vladyslav.ponomarov@workday.com>
2024-07-09 01:59:45 +0200

    upd allowed_hosts

--- Kralovska_Kava/Kralovska_Kava/settings.py
+++ Kralovska_Kava/Kralovska_Kava/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['35.208.49.205', ]
+ALLOWED_HOSTS = ['35.208.49.205', "kavakral.store"]
 
 
 # Application definition
